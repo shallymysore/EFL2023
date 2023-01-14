0e6b1905046f54e9dd2c41332b8e321bd081db6e
shallymysore <bkshashankhr@gmail.com>
2023-01-14 00:12:04 -0800

    Update app.py

--- app.py
+++ app.py
@@ -6,16 +6,16 @@ import json
 
 app = Flask(__name__)
 
-client = MongoClient("mongodb+srv://efladmin:god_is_watching@cluster0.eezohvz.mongodb.net/?retryWrites=true&w=majority")
+#client = MongoClient("mongodb+srv://efladmin:god_is_watching@cluster0.eezohvz.mongodb.net/?retryWrites=true&w=majority")
 
-db=client["efl2023"]
+#db=client["efl2023"]
 
-collections = db["eflCricket"]
+#collections = db["eflCricket"]
 
 @app.route("/")
 def welcome():
     return "Welcome to EFL2023"
-
+'''
 @app.route('/getplayer', methods=["GET"])
 def get_player():
     tier1 = []
@@ -55,7 +55,7 @@ def update_player(_id):
     result = collections.update_one(filter, {"$set": updated_data})
    
     return json_util.dumps(result.raw_result)
-
+'''
 if __name__ == '__main__':
     app.run()
     
