8e524b1fc0e02212016af41552881378c2523e4e
shallymysore <bkshashankhr@gmail.com>
2023-01-29 00:11:51 -0800

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,8 @@ db=client["efl2023"]
 
 collections = db["eflCricket"]
 
+ownercollection = db["eflCricketOwners"]
+
 @app.route("/")
 def welcome():
     return "Welcome to EFL2023"
@@ -24,6 +26,14 @@ def get_all_players():
     for player in cursor:
         players.append(player)
     return json.loads(json_util.dumps(players))
+
+@app.route('/getallownersdata', methods=["GET"])
+def get_all_owners():
+    owners = []
+    cursor = ownercollection.find()
+    for owner in cursor:
+        owners.append(owner)
+    return json.loads(json_util.dumps(owners))
 
 @app.route('/getplayer', methods=["GET"])
 def get_player():
@@ -62,6 +72,37 @@ def update_player(_id):
     filter = {"_id": ObjectId(str(_id))}
    
     result = collections.update_one(filter, {"$set": updated_data})
+    
+    #code to handle second owner update
+    if updated_data['status'] == "sold":
+        owner_team = updated_data['ownerTeam']
+
+        myquery = {"ownerName":owner_team}
+
+        owners_data = ownercollection.find(myquery)
+
+        for owner_items in owners_data:
+            owner_items["currentPurse"] = owner_items["currentPurse"] - int(updated_data["boughtFor"])
+            owner_items["totalCount"] = owner_items["totalCount"] + 1
+            owner_items["maxBid"] = owner_items["currentPurse"] - (35 * (15-owner_items["totalCount"]))
+            if updated_data["role"] == "Batter":
+                owner_items["batCount"] = owner_items["batCount"] + 1
+            elif updated_data["role"] == "Bowler":
+                owner_items["ballCount"] = owner_items["ballCount"] + 1
+            elif updated_data["role"] == "Allrounder":
+                owner_items["batCount"] = owner_items["batCount"] + 1
+                owner_items["ballCount"] = owner_items["ballCount"] + 1
+            elif updated_data["role"] == "WK-Batter":
+                owner_items["batCount"] = owner_items["batCount"] + 1
+                owner_items["wkCount"] = owner_items["wkCount"] + 1
+            else:
+                print("Role not found")
+            
+            if updated_data["country"] != "India":
+                owner_items["fCount"] = owner_items["fCount"] + 1
+
+        filter_owner = {"_id": ObjectId(str(owner_items["_id"]))}
+        result_owner = ownercollection.update_one(filter_owner, {"$set": owner_items})
    
     return json_util.dumps(result.raw_result)
 
